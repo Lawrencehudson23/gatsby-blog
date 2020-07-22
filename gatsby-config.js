9f208ab2c5d853d665ad0eaeefba1afdfe91f630
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-07-22 02:58:32 -0700

    added gatsby-transformer-remark to the app

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
         path: `${__dirname}/src/markdown-pages`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
